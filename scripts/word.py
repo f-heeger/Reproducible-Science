8cb63b5a5c2e5de292a9b6a517ba5928325d84dc
Thomas Titscher <thomas.titscher@bam.de>
2020-09-22 12:37:37 +0200

    Add script to replace images in word

--- scripts/word.py
+++ scripts/word.py
@@ -2,9 +2,50 @@
 Interacting with MS Word
 ------------------------
 
-Requires ``python-docx`` e.g. ``pip3 install docx``
+One use case in reproducible science that works great in the context of
+LaTeX files is the separation of text and images, where the images are
+generated automatically in separate workflows. For MS Word, the challenge
+is to update images of a document automatically.
 
+A MS Word document is basically a zip file containing a text layer and a 
+picture layer. Those pictures are, if unzipped, stored in word/media and can 
+be accessed via
+
+"""
+import zipfile
+
+z = zipfile.ZipFile("report.docx")
+all_files = z.namelist()
+images = [f for f in all_files if f.startswith("word/media/")]
+print(images)
+
+"""
+You can now modify the contents (actually creating a new zip file) to replace 
+one of the images with another one. This is conveniently done by the provided
+``class UpdatableZipFile`` (web search: _python replace file in zip_ lead to 
+https://stackoverflow.com/a/35435548 )
+
+You can direcly overwrite the *report.docx*, but we chose to work on a copy of
+it (*report_modified.docx*) here.
 
 """
 
-import docx
+from upgradable_zip_file import *
+
+shutil.copy("report.docx", "report_modified.docx")
+
+new_images = [ # order matters!
+    "dummy_images/red.png",
+    "dummy_images/blue.png",
+    "dummy_images/gray.png",
+    "dummy_images/green.png",
+]
+
+with UpdateableZipFile("report_modified.docx", "a") as o:
+    for new, old in zip(new_images, images):
+        o.write(new, old)
+
+"""
+The big inconvenience is that you cannot recover the image names from 
+the \*.docx file, so you have to rely on the ordering.
+"""
